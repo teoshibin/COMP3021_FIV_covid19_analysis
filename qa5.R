d72737907334cef99bdf54a8675feb57f04caaff
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2022-03-10 01:55:00 +0000

    completed GIF

--- qa5.R
+++ qa5.R
@@ -1,15 +1,69 @@
 
-library(ggplot2)
-library(dplyr)
-library(maps)
-library(gifski)
-library(gtools)
+# Installs pacman package if needed
+if (!require("pacman")) install.packages("pacman")
+
+# load packages
+pacman::p_load(
+    pacman,     # package installer
+    installr,   # software installer
+    rio,        # data importing
+    dplyr,      # data manipulation
+    tidyr,      # data manipulation (gather etc.)
+    ggplot2,    # ploting
+    stringr,    # string formatting
+    scales,     # plot scales and color etc. 
+    viridis,    # color palette
+    hrbrthemes, # plot themes
+    plotly,     # interactive plot
+    treemap,    # tree plot
+    htmlwidgets, # save interactive plot into html
+    maps,
+    gifski,
+    gtools
+)
+
+# This is needed if packages are giving errors for incompatible binary distribution
+# install.Rtools() 
+
+devtools::install_github("timelyportfolio/d3treeR")
+library(d3treeR) # interactive tree plot
+
+# load custom functions
+source("./functions/general_functions.R")
+
+# inputs
+dataset_path <- "./Data/"
+function_path <- "./Functions/"
+
+# outputs
+itrt_plot_path <- "./Output/InteractivePlot/"
+sttc_plot_path <- "./Output/StaticPlot/"
+out_data_path <- "./Output/Data/"
+
+daily_covid <- import(
+    paste0(dataset_path, "worldometer_coronavirus_daily_data.csv")
+)
+
+summary_covid <- import(
+    paste0(dataset_path, "worldometer_coronavirus_summary_data.csv")
+)
+
+head(daily_covid)
+head(summary_covid)
+
+daily_covid <-
+    daily_covid %>%
+    replace(is.na(.), 0) %>% 
+    mutate(date = as.Date(date))
+
+###############################################################################
 
 # data
 world <- map_data("world")
 
 # loop config
-date_seq <- seq(as.Date(min(daily_covid$date)), as.Date(max(daily_covid$date)), "days")
+date_seq <- seq(as.Date(min(daily_covid$date)), 
+                as.Date(max(daily_covid$date)), "days")
 print_frequrency <- 50
 len_date_seq <- length(date_seq)
 
@@ -75,10 +129,11 @@ for (i in c(1:len_date_seq)) {
     
 }
 
-# load png paths and convert it into gif
+# load png paths and convert it into GIF
 png_files <- list.files(paste0(itrt_plot_path, "qa5_map/"), 
                         pattern = ".*png$", full.names = TRUE)
 png_files <- mixedsort(sort(png_files))
 gifski(png_files, gif_file = paste0(itrt_plot_path, "qa5_map_08fps.gif"), 
        width = 1920, height = 1065, delay = 0.125)
 
+pacman::p_unload(all)
