471f633a16d4a08c74d489da4345ae01956aeefb
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2022-03-11 02:02:41 +0000

    final plot

--- Functions/general_functions.R
+++ Functions/general_functions.R
@@ -6,3 +6,16 @@
 # commaNum <- function(num) {
 #     return(prettyNum(num, big.mark = ",", scientific = F))
 # }
+
+myShrinkNum <- function(num){
+    if (num >= 1e9) {
+        return(patse(round(num/1e9, 2), "B"))
+    }
+    if (num >= 1e6) {
+        return(paste(round(num/1e6, 2), "M"))
+    }
+    if (num >= 1e3) {
+        return(paste(round(num/1e3, 2), "K"))
+    }
+    return(paste0(num))
+}
